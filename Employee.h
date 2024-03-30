3400945379e5d8660748b8040c42a9a37a9ab762
John Hobbs <165341471+JohnAVHobbs@users.noreply.github.com>
2024-03-30 18:38:29 -0500

    Create Employee.h
    The header file for the Employee class

---
+++ Employee.h
@@ -0,0 +1,24 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include<string>
+using namespace std;
+class Employee
+{
+  protected:
+    string name;
+    double wage;
+    double hours;
+
+  public:
+    // Create a new employee
+    Employee (string theName, double theWage, double theHours);
+
+    // Calculate the employee’s pay.
+    double calcPay() const;
+
+    //get the employee's name
+    string getName () const;
+};
+
+#endif // EMPLOYEE_H
